bfa9c6dc6117a13dcbe00afd037661568cf9c69a
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2016-12-06 08:36:45 +0200

    Day 6 solution

--- day6/run.go
+++ day6/run.go
@@ -1,10 +1,50 @@
 package day6
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
+
+func mostCommonChar(s string) string {
+	m := make(map[rune]int)
+	for _, ch := range s {
+		if _, exists := m[ch]; !exists {
+			m[ch] = 0
+		}
+		m[ch]++
+	}
+	max := 0
+	var result rune
+	for ch, n := range m {
+		if max < n {
+			max = n
+			result = ch
+		}
+	}
+	return string(result)
+}
+
+func leastCommonChar(s string) string {
+	m := make(map[rune]int)
+	for _, ch := range s {
+		if _, exists := m[ch]; !exists {
+			m[ch] = 0
+		}
+		m[ch]++
+	}
+	min := len(s)
+	var result rune
+	for ch, n := range m {
+		if min > n {
+			min = n
+			result = ch
+		}
+	}
+	return string(result)
+}
 
 func Run(part int) {
 	if len(os.Args) != 4 {
@@ -16,11 +56,33 @@ func Run(part int) {
 	if err != nil {
 		log.Fatalf("Failed to read input file - %s", err)
 	}
-	_ = data
 
 	switch part {
 	case 1:
+		lines := strings.Split(string(data), "\n")
+		columns := make([]string, len(lines[0]))
+		for _, line := range lines {
+			for i, ch := range line {
+				columns[i] = fmt.Sprintf("%s%s", columns[i], string(ch))
+			}
+		}
+		result := ""
+		for _, col := range columns {
+			result = fmt.Sprintf("%s%s", result, mostCommonChar(col))
+		}
+		log.Printf("Error-corrected version of the message is '%s'", result)
 	case 2:
-		log.Print("Part 2 is not implemented yet")
+		lines := strings.Split(string(data), "\n")
+		columns := make([]string, len(lines[0]))
+		for _, line := range lines {
+			for i, ch := range line {
+				columns[i] = fmt.Sprintf("%s%s", columns[i], string(ch))
+			}
+		}
+		result := ""
+		for _, col := range columns {
+			result = fmt.Sprintf("%s%s", result, leastCommonChar(col))
+		}
+		log.Printf("Error-corrected version of the message is '%s'", result)
 	}
 }
